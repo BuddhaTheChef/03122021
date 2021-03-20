b8b196f2f8874d342af69800ac0c0f95d97fafc4
Aj Wietecha <ajwietechaii@yahoo.com>
2021-03-20 13:16:06 -0700

    added in new message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input)
+  console.log(messages)
+
+  const sendMessage = (event) => {
+    // all logic to send message goes here
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
      <h1>Adding content</h1>
+     <input value={input} onChange={event => setInput(event.target.value)}/>
+     <button onClick={sendMessage}>Send Message</button>
+
+     {messages.map(message => (
+       <p>{message}</p>
+     ))}
       </header>
     </div>
   );
